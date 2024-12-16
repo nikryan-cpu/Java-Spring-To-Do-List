caea8087911ab27f922e392c9650b95449ae16cc
Nik Kur <nik.kur0109@gmail.com>
2024-12-16 11:47:04 +0300

    Added contact us and more info endpoints, visual updates

--- toDoList/src/main/java/com/todolist/todolist/controllers/MainController.java
+++ toDoList/src/main/java/com/todolist/todolist/controllers/MainController.java
@@ -1,16 +1,18 @@
 package com.todolist.todolist.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.security.Principal;
 
 @RestController
-@RequestMapping("/secured")
 public class MainController {
-    @GetMapping("/user")
-    public String user(Principal principal) {
-        return principal.getName();
+    @GetMapping("/more-info")
+    public ModelAndView moreInfo() {
+        return new ModelAndView("more-info");
+    }
+    @GetMapping("/contact")
+    public ModelAndView contact() {
+        return new ModelAndView("contact");
     }
 }
